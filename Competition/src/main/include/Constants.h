4e4acade103f66a4a0822075302eae724693366b
Rohit Chawla <rohitchawla77@gmail.com>
2020-06-05 00:23:03 -0500

    drive subsystem code including ramsete functionality methods

--- Competition/src/main/include/Constants.h
+++ Competition/src/main/include/Constants.h
@@ -20,3 +20,76 @@
 namespace OIConstants {
 constexpr int kDriverControllerPort = 1;
 }  // namespace OIConstants
+
+namespace DriveConstants {
+
+    // 1/2/3, 4/5/6 from pdp increasing towards intake
+    constexpr static int CAN_ID_LEFT_LEAD = 1;
+    constexpr static int CAN_ID_LEFT_FOLLOW_A = 2;
+    constexpr static int CAN_ID_LEFT_FOLLOW_B = 3;
+    constexpr static int CAN_ID_RIGHT_LEAD = 4;
+    constexpr static int CAN_ID_RIGHT_FOLLOW_A = 5;
+    constexpr static int CAN_ID_RIGHT_FOLLOW_B = 6;
+
+    constexpr auto kTrackwidth = 0.68_m;
+
+    constexpr double kWheelDiameterInches = 6;
+    constexpr double kGearRatio = 8.8;
+
+    constexpr static double kP = -0.01;
+    constexpr static double kI = 0;
+    constexpr static double kD = 0;
+    constexpr static double kFF = 0.00017543859;
+    constexpr static double kIz = 0;
+    constexpr static double MAX_RPM = 5700;
+    constexpr static double kMinOutput = -1.0;
+    constexpr static double kMaxOutput = 1.0;
+    constexpr static double kMinVel = 0;
+    constexpr static double kMaxVel = 1000;
+    constexpr static double kMaxAccel = 1500;
+    constexpr static double kAllError = 0;
+    constexpr static double kDeadband = 0.05;
+    // changed x and y deadband to .05 for testing
+    constexpr static double kDeadbandY = 0.05;
+    constexpr static double kDeadbandX = 0.05;
+    constexpr static double kDriveMultiplierX = 0.5;
+    constexpr static double kDriveMultiplierY = 1;
+    constexpr static double kDriveOffset = 1;
+
+}
+
+namespace RamseteConstants {
+
+    constexpr auto kMaxSpeed = 4.4_mps; // 4_mps
+    constexpr auto kMaxAcceleration = 4_mps_sq; //4_mps_sq;
+
+    constexpr double kRamseteB = 2;
+    constexpr double kRamseteZeta = 0.7;
+
+    // Convert rotations to meters using gear ratio and wheel diameter (converted to meters)
+    constexpr double kPositionConversionFactor = DriveConstants::kWheelDiameterInches * 0.0254 / DriveConstants::kGearRatio * M_PI;
+    // Convert rotations per minute to meters per second 
+    constexpr double kVelocityConversionFactor = kPositionConversionFactor / 60;
+
+    constexpr bool kGyroReversed = true;
+
+    constexpr auto kS = 0.146_V;
+    constexpr auto kV = 2.25 * 1_V * 1_s / 1_m;
+    constexpr auto kA = 0.315 * 1_V * 1_s * 1_s / 1_m;
+
+    constexpr double kPDriveVel = 3.09;
+
+    constexpr units::length::meter_t  kStartPos = 4.9_m;
+    //constexpr units::deg_t kStartAngularOffset = 15_deg;
+    constexpr units::length::meter_t  kCenterline = 0.94_m;
+
+}
+
+namespace LimelightConstants {
+    constexpr static int LED_MODE_ON = 3;
+    constexpr static int LED_MODE_OFF = 1;
+    constexpr static int TRACK_MODE_ON = 0;
+    constexpr static int TRACK_MODE_OFF = 1;
+}
+
+
