95834839a4b53b89d53be08f678a2750b3f6e106
Rohit Chawla <rohitchawla77@gmail.com>
2020-08-19 10:44:03 -0500

    started working on drivetrain with new structure

---
+++ Competition/src/main/cpp/Drivetrain2.cpp
@@ -0,0 +1,143 @@
+#include "Drivetrain2.h"
+
+Drivetrain2::Drivetrain2(frc::XboxController* controller) : driverController(controller),
+                                                            kDriveKinematics{DriveConstants::kTrackwidth},
+                                                            kSimpleMotorFeedforward{RamseteConstants::kS, RamseteConstants::kV, RamseteConstants::kA},
+                                                            kTrajectoryConfigF{RamseteConstants::kMaxSpeed, RamseteConstants::kMaxAcceleration},
+                                                            kTrajectoryConfigR{RamseteConstants::kMaxSpeed, RamseteConstants::kMaxAcceleration},
+                                                            kDifferentialDriveVoltageConstraint{kSimpleMotorFeedforward, kDriveKinematics, 10_V},
+                                                            leftDriveLead{DriveConstants::CAN_ID_LEFT_LEAD, rev::CANSparkMax::MotorType::kBrushless},
+                                                            leftDriveFollowA{DriveConstants::CAN_ID_LEFT_FOLLOW_A, rev::CANSparkMax::MotorType::kBrushless},
+                                                            leftDriveFollowB{DriveConstants::CAN_ID_LEFT_FOLLOW_B, rev::CANSparkMax::MotorType::kBrushless},
+                                                            rightDriveLead{DriveConstants::CAN_ID_RIGHT_LEAD, rev::CANSparkMax::MotorType::kBrushless},
+                                                            rightDriveFollowA{DriveConstants::CAN_ID_RIGHT_FOLLOW_A, rev::CANSparkMax::MotorType::kBrushless},
+                                                            rightDriveFollowB{DriveConstants::CAN_ID_RIGHT_FOLLOW_B, rev::CANSparkMax::MotorType::kBrushless},
+                                                            odometry{frc::Rotation2d(units::degree_t(getHeading()))} {
+    
+}
+
+void Drivetrain2::initDrivetrain() {
+    leftDriveLead.RestoreFactoryDefaults();
+    leftDriveFollowA.RestoreFactoryDefaults();
+    leftDriveFollowB.RestoreFactoryDefaults();
+    rightDriveLead.RestoreFactoryDefaults();
+    rightDriveFollowA.RestoreFactoryDefaults();
+    rightDriveFollowB.RestoreFactoryDefaults();
+
+    leftDriveLead.SetIdleMode(rev::CANSparkMax::IdleMode::kCoast);
+    leftDriveFollowA.SetIdleMode(rev::CANSparkMax::IdleMode::kCoast);
+    leftDriveFollowB.SetIdleMode(rev::CANSparkMax::IdleMode::kCoast);
+    rightDriveLead.SetIdleMode(rev::CANSparkMax::IdleMode::kCoast);
+    rightDriveFollowA.SetIdleMode(rev::CANSparkMax::IdleMode::kCoast);
+    rightDriveFollowB.SetIdleMode(rev::CANSparkMax::IdleMode::kCoast);
+
+    leftDriveLead.Follow(rev::CANSparkMax::kFollowerDisabled, false);
+    rightDriveLead.Follow(rev::CANSparkMax::kFollowerDisabled, false);
+
+    leftDriveFollowA.Follow(leftDriveLead);
+    leftDriveFollowB.Follow(leftDriveLead);
+
+    rightDriveFollowA.Follow(rightDriveLead);
+    rightDriveFollowB.Follow(rightDriveLead);
+
+    leftPIDController.SetP(DriveConstants::kP);
+    leftPIDController.SetI(DriveConstants::kI);
+    leftPIDController.SetD(DriveConstants::kD);
+    leftPIDController.SetIZone(DriveConstants::kIz);
+    leftPIDController.SetFF(DriveConstants::kFF);
+    leftPIDController.SetOutputRange(DriveConstants::kMinOutput, DriveConstants::kMaxOutput);
+
+    rightPIDController.SetP(DriveConstants::kP);
+    rightPIDController.SetI(DriveConstants::kI);
+    rightPIDController.SetD(DriveConstants::kD);
+    rightPIDController.SetIZone(DriveConstants::kIz);
+    rightPIDController.SetFF(DriveConstants::kFF);
+    rightPIDController.SetOutputRange(DriveConstants::kMinOutput, DriveConstants::kMaxOutput);
+
+    rightPIDController.SetSmartMotionMaxVelocity(DriveConstants::kMaxVel);
+    rightPIDController.SetSmartMotionMinOutputVelocity(DriveConstants::kMinVel);
+    rightPIDController.SetSmartMotionMaxAccel(DriveConstants::kMaxAccel);
+    rightPIDController.SetSmartMotionAllowedClosedLoopError(DriveConstants::kAllError);
+    
+    leftPIDController.SetSmartMotionMaxVelocity(DriveConstants::kMaxVel);
+    leftPIDController.SetSmartMotionMinOutputVelocity(DriveConstants::kMinVel);
+    leftPIDController.SetSmartMotionMaxAccel(DriveConstants::kMaxAccel);
+    leftPIDController.SetSmartMotionAllowedClosedLoopError(DriveConstants::kAllError);
+
+    leftDriveLead.SetInverted(false);
+    rightDriveLead.SetInverted(true);
+}
+
+void Drivetrain2::setDefaultState() {
+    state.drivetrainState = DrivetrainState::DISABLED;
+
+    resetState();
+}
+
+void Drivetrain2::assessInputs() {
+    if (driverController->GetTriggerAxis(frc::GenericHID::kLeftHand) > 0.05 ||
+        driverController->GetTriggerAxis(frc::GenericHID::kRightHand) > 0.05 ||
+        std::abs(driverController->GetX(frc::GenericHID::kLeftHand)) > 0.05 ||
+        driverController->GetYButton()) {
+        state.drivetrainState = DrivetrainState::MANUAL;
+    }
+}
+
+void Drivetrain2::assignOutputs() {
+    // move to assessInputs()?
+    odometry.Update(frc::Rotation2d(units::degree_t(getHeading())), getLeftDistance(), getRightDistance());
+}
+
+void Drivetrain2::resetState() {
+    resetEncoders();
+    resetOdometry(frc::Pose2d(0_m, 0_m, frc::Rotation2d(0_deg)));
+    resetIMU();
+
+    state.limelightState = LimelightConstants::STATE_ON; // Init as on so it immediately will turn off in the state machine
+}
+
+void Drivetrain2::resetEncoders() {
+    leftCANEncoder.SetPosition(0);
+    rightCANEncoder.SetPosition(0);
+}
+
+void Drivetrain2::resetOdometry(frc::Pose2d pose) {
+    resetEncoders();
+    odometry.ResetPosition(pose, frc::Rotation2d(units::degree_t(getHeading())));
+}
+
+void Drivetrain2::resetIMU() {
+    imu.Reset();
+}
+
+units::meter_t Drivetrain2::getLeftDistance() {
+    return leftCANEncoder.GetPosition() * RamseteConstants::kPositionConversionFactor * 1_m;
+}
+
+units::meter_t Drivetrain2::getRightDistance() {
+    return rightCANEncoder.GetPosition() * RamseteConstants::kPositionConversionFactor * 1_m;
+}
+
+// check if the correct GetHeading was called last year
+double Drivetrain2::getHeading() {
+    return std::remainder(imu.GetAngle(), 360) * (RamseteConstants::kGyroReversed ? -1.0 : 1.0);
+}
+
+double Drivetrain2::getTurnRate() {
+    return imu.GetRate() * (RamseteConstants::kGyroReversed ? -1.0 : 1.0);
+}
+
+frc::Pose2d Drivetrain2::getPose() {
+    return odometry.GetPose();
+}
+
+frc::DifferentialDriveWheelSpeeds Drivetrain2::getWheelSpeeds() {
+    return {units::meters_per_second_t(leftCANEncoder.GetVelocity() * RamseteConstants::kVelocityConversionFactor),
+            units::meters_per_second_t(rightCANEncoder.GetVelocity() * RamseteConstants::kVelocityConversionFactor)};
+}
+
+void Drivetrain2::tankDriveVolts(units::volt_t leftVolts, units::volt_t rightVolts) {
+    leftDriveLead.SetVoltage(leftVolts);
+    rightDriveLead.SetVoltage(rightVolts);
+}
+
---
+++ Competition/src/main/include/Drivetrain2.h
@@ -0,0 +1,82 @@
+#pragma once
+
+#include "ValorSubsystem.h"
+#include "Constants.h"
+
+#include <frc/XboxController.h>
+#include <frc/geometry/Pose2d.h>
+#include <frc/kinematics/DifferentialDriveOdometry.h>
+#include <units/units.h>
+#include <frc/kinematics/DifferentialDriveWheelSpeeds.h>
+#include <frc/kinematics/DifferentialDriveKinematics.h>
+#include <adi/ADIS16448_IMU.h>
+#include <frc/controller/SimpleMotorFeedforward.h>
+#include <frc/trajectory/TrajectoryConfig.h>
+#include <frc/trajectory/constraint/DifferentialDriveVoltageConstraint.h>
+#include <rev/CANSparkMax.h>
+#include <rev/CANEncoder.h>
+#include <rev/CANPIDController.h>
+
+class Drivetrain2 : public ValorSubsystem {
+    public:
+        Drivetrain2(frc::XboxController*);
+
+        void initDrivetrain();
+
+        void setDefaultState();
+        void assessInputs();
+        void assignOutputs();
+
+        void resetState();
+
+        // ramsete methods
+        void resetEncoders();
+        void resetOdometry(frc::Pose2d pose);
+        void resetIMU();
+
+        units::meter_t getLeftDistance();
+        units::meter_t getRightDistance();
+        double getHeading();
+        double getTurnRate();
+        frc::Pose2d getPose();
+        frc::DifferentialDriveWheelSpeeds getWheelSpeeds();
+        void tankDriveVolts(units::volt_t leftVolts, units::volt_t rightVolts);
+
+        enum DrivetrainState {
+            DISABLED,
+            AUTO,
+            MANUAL
+        };
+
+        struct x {
+            DrivetrainState drivetrainState;
+            double currentLeftPower;
+            double currentRightPower;
+            int limelightState;
+        } state;
+
+        frc::DifferentialDriveKinematics kDriveKinematics;
+        frc::SimpleMotorFeedforward<units::meters> kSimpleMotorFeedforward;
+        frc::TrajectoryConfig kTrajectoryConfigF;
+        frc::TrajectoryConfig kTrajectoryConfigR;
+        frc::DifferentialDriveVoltageConstraint kDifferentialDriveVoltageConstraint;
+
+    private:
+        rev::CANSparkMax leftDriveLead;
+        rev::CANSparkMax leftDriveFollowA;
+        rev::CANSparkMax leftDriveFollowB;
+        rev::CANSparkMax rightDriveLead;
+        rev::CANSparkMax rightDriveFollowA;
+        rev::CANSparkMax rightDriveFollowB;
+
+        rev::CANPIDController leftPIDController = leftDriveLead.GetPIDController();
+        rev::CANPIDController rightPIDController = rightDriveLead.GetPIDController();
+        rev::CANEncoder leftCANEncoder = leftDriveLead.GetEncoder();
+        rev::CANEncoder rightCANEncoder = rightDriveLead.GetEncoder();
+
+        frc::ADIS16448_IMU imu{};
+
+        frc::DifferentialDriveOdometry odometry;
+
+        frc::XboxController* driverController;
+};
