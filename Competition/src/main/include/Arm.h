b0a7af0172e48e7ff710b68cd9e44bafa09b86a3
Rohit Chawla <rohitchawla77@gmail.com>
2020-09-09 18:09:19 -0500

    arm updates

--- Competition/src/main/include/Arm.h
+++ Competition/src/main/include/Arm.h
@@ -1,3 +1,10 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
 #pragma once
 
 #include "ValorSubsystem.h"
@@ -15,13 +22,14 @@ class Arm : public ValorSubsystem {
         Arm();
         void setController(frc::XboxController*);
 
-        void initArm();
+        void init();
 
         void setDefaultState();
         void assessInputs();
         void assignOutputs();
 
         void setDisengage(bool disengage);
+        
         void resetState();
 
         enum ArmState {
