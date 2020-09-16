8235dc4b608cdd082273ec99235849dece12c343
Cooper <cjnelson016@gmail.com>
2020-09-16 18:29:36 -0500

    added hopper controllers to ConfigureButtonBindings

--- Competition/src/main/cpp/RobotContainer.cpp
+++ Competition/src/main/cpp/RobotContainer.cpp
@@ -17,6 +17,7 @@ RobotContainer::RobotContainer() {
 void RobotContainer::ConfigureButtonBindings() {
     m_drivetrain.setController(&m_GamepadDriver);
     m_arm.setController(&m_GamepadOperator);
+    m_hopper.setController(&m_GamepadOperator, &m_GamepadOperator);
 }
 
 frc2::Command* RobotContainer::GetAutonomousCommand() {
