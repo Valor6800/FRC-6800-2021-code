e2bda6c448949a3934f0c32bf25ec7d9b840586e
Rohit Chawla <rohitchawla77@gmail.com>
2020-09-25 17:43:27 -0500

    added register subsystem to hopper, fixed hopper logic

--- Competition/src/main/cpp/Hopper.cpp
+++ Competition/src/main/cpp/Hopper.cpp
@@ -1,65 +1,57 @@
-// #include "Hopper.h"
+#include "Hopper.h"
 
-// Hopper::Hopper() : ValorSubsystem(),
-//              hopperMtr{HopperConstants::VICTOR_ID_HOPPER}, 
-//              throatMtr{HopperConstants::VICTOR_ID_THROAT},
-//              operatorController(NULL),
-//              driverController(NULL) {
-// }
+Hopper::Hopper() : ValorSubsystem(),
+                   hopperMtr{HopperConstants::VICTOR_ID_HOPPER}, 
+                   throatMtr{HopperConstants::VICTOR_ID_THROAT},
+                   operatorController(NULL),
+                   driverController(NULL) {
+    frc2::CommandScheduler::GetInstance().RegisterSubsystem(this);
+}
 
-// void Hopper::setController(frc::XboxController* controllerOperator, frc::XboxController* controllerDriver) {
-//     operatorController = controllerOperator;
-//     driverController = controllerDriver;
-// }
+void Hopper::setController(frc::XboxController* controllerOperator, frc::XboxController* controllerDriver) {
+    operatorController = controllerOperator;
+    driverController = controllerDriver;
+}
 
-// void Hopper::init() {
-//     hopperMtr.SetInverted(false);
-    
-// }
+void Hopper::init() {
+    hopperMtr.SetInverted(false);
+}
 
-// void Hopper::setDefaultState() {
-//     resetState();
-// }
+void Hopper::setDefaultState() {
+    resetState();
+}
 
-// void Hopper::assessInputs() {
-//     // Prevent controller segfault
-//     if (!operatorController) {
-//         return;
-//     }
-//     if (!driverController) {
-//         return;
-//     }
-//     else if (operatorController->GetBumper(frc::GenericHID::kRightHand)) {
-//         state.hopperState = HopperState::REVERSE;
-//     }
-//     if (driverController->GetBumper(frc::GenericHID::kLeftHand)) {
-//         state.hopperState = HopperState::FORWARD;
-//     }
-//     else {
-//         state.hopperState = HopperState::DISABLED;
-//     }
-    
-// }
+void Hopper::assessInputs() {
+    // Prevent controller segfault
+    if (!operatorController) {
+        return;
+    }
+    if (!driverController) {
+        return;
+    }
 
-// void Hopper::assignOutputs() {
-//     if (state.hopperState == HopperState::FORWARD) {
-//         state.currentHopperPower = 1.0;
-//         state.currentThroatPower = 1.0;
+    if (driverController->GetBumper(frc::GenericHID::kLeftHand)) {
+        state.hopperState = HopperState::FORWARD;
+    }
+    else {
+        state.hopperState = HopperState::DISABLED;
+    }
+}
 
-//     }
-//     else if (state.hopperState == HopperState::REVERSE) {
-//             state.currentHopperPower = -1.0;
-//             state.currentThroatPower = -1.0;
-
-//     }
-//     else  {
-//             state.currentHopperPower = 0;
-//             state.currentThroatPower = 0;
-//     }    
+void Hopper::assignOutputs() {
+    if (state.hopperState == HopperState::FORWARD) {
+        state.currentHopperPower = 0.8;
+        state.currentThroatPower = 0.8;
+    }
+    else  {
+        state.currentHopperPower = 0;
+        state.currentThroatPower = 0;
+    }    
       
-//     hopperMtr.Set(state.currentHopperPower);
-//     throatMtr.Set(state.currentThroatPower);
-// }
-// void Hopper::resetState() {
-//     state.hopperState = HopperState::DISABLED;
-// }
+    hopperMtr.Set(state.currentHopperPower);
+    throatMtr.Set(state.currentThroatPower);
+}
+
+void Hopper::resetState() {
+    state.hopperState = HopperState::DISABLED;
+}
--- Competition/src/main/include/Hopper.h
+++ Competition/src/main/include/Hopper.h
@@ -1,52 +1,45 @@
-// #pragma once
+#pragma once
 
-// #include "ValorSubsystem.h"
-// #include "Constants.h"
+#include "ValorSubsystem.h"
+#include "Constants.h"
 
-// #include <frc/XboxController.h>
-// #include <frc/PWMVictorSPX.h>
-// #include <frc/Timer.h>
+#include <frc/XboxController.h>
+#include <frc/PWMVictorSPX.h>
+#include <frc/Timer.h>
 
-// #ifndef HOPPER_H
-// #define HOPPER_H
+#ifndef HOPPER_H
+#define HOPPER_H
 
-// class Hopper : public ValorSubsystem {
-//     public:
-//         Hopper();
-//         void setController(frc::XboxController*, frc::XboxController*);
+class Hopper : public ValorSubsystem {
+    public:
+        Hopper();
+        void setController(frc::XboxController*, frc::XboxController*);
         
+        void init();
+        void setDefaultState();
+        void assessInputs();
+        void assignOutputs();
 
-//         void init();
-//         void setDefaultState();
-//         void assessInputs();
-//         void assignOutputs();
+        void resetState();
 
-//         void resetState();
+        enum HopperState {
+            DISABLED, 
+            FORWARD
+        };
 
-//         enum HopperState {
-//             DISABLED, 
-//             FORWARD, 
-//             REVERSE
-//         };
-
-//         struct x {
-//             HopperState hopperState;
-
-//             //frc::Timer timer;
+        struct x {
+            HopperState hopperState;
             
-//             double currentHopperPower;
-//             double currentThroatPower;
-//             //double currentTime;
-            
+            double currentHopperPower;
+            double currentThroatPower;
+        } state;
 
-//         } state;
+    private:
+        frc::PWMVictorSPX hopperMtr;
+        frc::PWMVictorSPX throatMtr;
 
-//     private:
-//         frc::PWMVictorSPX hopperMtr;
-//         frc::PWMVictorSPX throatMtr;
+        frc::XboxController* operatorController;
+        frc::XboxController* driverController;
+};
 
-//         frc::XboxController* operatorController;
-//         frc::XboxController* driverController;
-// };
-
-// #endif
+#endif
