456362d817e7ce4d38fe022e177c6267eaeaae39
Rohit Chawla <rohitchawla77@gmail.com>
2020-09-25 17:42:30 -0500

    added register subsystem to intake, fixed intake logic

--- Competition/src/main/cpp/Intake.cpp
+++ Competition/src/main/cpp/Intake.cpp
@@ -1,63 +1,59 @@
-// #include "Intake.h"
+#include "Intake.h"
 
-// Intake::Intake() : ValorSubsystem(),
-//              intakeMtr{IntakeConstants::VICTOR_ID_INTAKE}, 
-//              operatorController(NULL),
-//              driverController(NULL) {
-    
-// }
+Intake::Intake() : ValorSubsystem(),
+                   intakeMtr{IntakeConstants::VICTOR_ID_INTAKE}, 
+                   operatorController(NULL),
+                   driverController(NULL) {
+    frc2::CommandScheduler::GetInstance().RegisterSubsystem(this);
+}
 
-// void Intake::setController(frc::XboxController* controllerOperator, frc::XboxController* controllerDriver) {
-//     operatorController = controllerOperator;
-//     driverController = controllerDriver;
-// }
+void Intake::setController(frc::XboxController* controllerOperator, frc::XboxController* controllerDriver) {
+    operatorController = controllerOperator;
+    driverController = controllerDriver;
+}
 
-// void Intake::init() {
-//     intakeMtr.SetInverted(false);
-    
-// }
+void Intake::init() {
+    intakeMtr.SetInverted(false);
+}
 
-// void Intake::setDefaultState() {    
-//     resetState();
-// }
+void Intake::setDefaultState() {    
+    resetState();
+}
 
-// void Intake::assessInputs() {
-//     // Prevent controller segfault
-//     if (!operatorController) {
-//         return;
-//     }
-//     if (!driverController) {
-//         return;
-//     }
-//     if (operatorController->GetBumper(frc::GenericHID::kLeftHand)) {
-//         state.intakeState = IntakeState::IN;
-//     }
-//     else if (driverController->GetBumper(frc::GenericHID::kLeftHand)) {
-//         state.intakeState = IntakeState::OUT;
-//     }
-//     else {
-//         state.intakeState = IntakeState::DISABLED;
-//     }
-    
-// }
+void Intake::assessInputs() {
+    // Prevent controller segfault
+    if (!operatorController) {
+        return;
+    }
+    if (!driverController) {
+        return;
+    }
 
-// void Intake::assignOutputs() {
-//     if (state.intakeState == IntakeState::IN) {
-//         state.currentPower = 1.0;
-        
+    if (operatorController->GetBumper(frc::GenericHID::kLeftHand) || driverController->GetBumper(frc::GenericHID::kLeftHand)) {
+        state.intakeState = IntakeState::IN;
+    }
+    else if (operatorController->GetBumper(frc::GenericHID::kRightHand)) {
+        state.intakeState = IntakeState::OUT;
+    }
+    else {
+        state.intakeState = IntakeState::DISABLED;
+    }
+}
 
-//     }
-//     else if (state.intakeState == IntakeState::OUT) {
-//             state.currentPower = -1.0;
-            
+void Intake::assignOutputs() {
+    if (state.intakeState == IntakeState::IN) {
+        state.currentPower = 0.75;
+    }
+    else if (state.intakeState == IntakeState::OUT) {
+        state.currentPower = -0.75;
+    }
+    else {
+        state.currentPower = 0;  
+    }
 
-//     }
-//     else  {
-//             state.currentPower = 0;  
-//           }    
-      
-//     intakeMtr.Set(state.currentPower);
-// }
-// void Intake::resetState() {
-//     state.intakeState = IntakeState::DISABLED;
-// }
+    intakeMtr.Set(state.currentPower);
+}
+
+void Intake::resetState() {
+    state.intakeState = IntakeState::DISABLED;
+}
