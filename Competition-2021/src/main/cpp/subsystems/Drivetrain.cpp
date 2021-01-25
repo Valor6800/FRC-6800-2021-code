5fd5fa58b1a79bebf15c28eef9281caadedb4ff7
dkasabovn <dkasabovn@gmail.com>
2021-01-25 16:46:45 -0600

    upgrade to 2021

---
+++ Competition-2021/src/main/cpp/subsystems/Drivetrain.cpp
@@ -0,0 +1,153 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "subsystems/Drivetrain.h"
+
+// move driveModeState check out of assessiNputs?
+// how to set state manual?
+
+Drivetrain::Drivetrain() : ValorSubsystem(),
+                           leftDriveLead{DriveConstants::CAN_ID_LEFT_A, rev::CANSparkMax::MotorType::kBrushless},
+                           leftDriveFollow{DriveConstants::CAN_ID_LEFT_B, rev::CANSparkMax::MotorType::kBrushless},
+                           rightDriveLead{DriveConstants::CAN_ID_RIGHT_A, rev::CANSparkMax::MotorType::kBrushless},
+                           rightDriveFollow{DriveConstants::CAN_ID_RIGHT_B, rev::CANSparkMax::MotorType::kBrushless},
+                           driverController(NULL) {
+    frc2::CommandScheduler::GetInstance().RegisterSubsystem(this);
+    init();
+}
+
+void Drivetrain::init() {
+    leftDriveLead.RestoreFactoryDefaults();
+    leftDriveFollow.RestoreFactoryDefaults();
+    rightDriveLead.RestoreFactoryDefaults();
+    rightDriveFollow.RestoreFactoryDefaults();
+
+    leftDriveLead.SetIdleMode(rev::CANSparkMax::IdleMode::kCoast);
+    leftDriveFollow.SetIdleMode(rev::CANSparkMax::IdleMode::kCoast);
+    rightDriveLead.SetIdleMode(rev::CANSparkMax::IdleMode::kCoast);
+    rightDriveFollow.SetIdleMode(rev::CANSparkMax::IdleMode::kCoast);
+
+    leftDriveLead.Follow(rev::CANSparkMax::kFollowerDisabled, false);
+    rightDriveLead.Follow(rev::CANSparkMax::kFollowerDisabled, false);
+
+    leftDriveFollow.Follow(leftDriveLead);
+
+    rightDriveFollow.Follow(rightDriveLead);
+
+    leftDriveLead.SetInverted(true);
+    rightDriveLead.SetInverted(false);
+}
+
+void Drivetrain::setController(frc::XboxController* controller) {
+    driverController = controller;
+}
+
+void Drivetrain::setDefaultState() {
+    state.drivetrainState = DrivetrainState::DISABLED;
+    state.driveModeState = DriveModeState::ROCKET_LEAGUE;
+
+    resetState();
+}
+
+void Drivetrain::setState(Drivetrain::DrivetrainState _state) {
+    state.drivetrainState = _state;
+}
+
+void Drivetrain::assessInputs() {
+    if (!driverController) {
+        return;
+    }
+
+    // drive mode
+    if (driverController->GetBackButtonPressed()) {
+        state.driveModeState = DriveModeState::ARCADE;
+    }
+    else if (driverController->GetStartButtonPressed()) {
+        state.driveModeState = DriveModeState::ROCKET_LEAGUE;
+    }
+
+    // driver inputs
+    state.leftStickY = driverController->GetY(frc::GenericHID::kLeftHand);
+    state.rightStickX = driverController->GetX(frc::GenericHID::kRightHand);
+    
+    state.leftTrigger = driverController->GetTriggerAxis(frc::GenericHID::kLeftHand);
+    state.rightTrigger = driverController->GetTriggerAxis(frc::GenericHID::kRightHand);
+    state.leftStickX = driverController->GetX(frc::GenericHID::kLeftHand);
+    state.yButton = driverController->GetYButton();
+    state.rightBumper = driverController->GetBumper(frc::GenericHID::kRightHand);
+}
+
+void Drivetrain::assignOutputs() {
+    // arcade
+    if (state.driveModeState == DriveModeState::ARCADE) {
+
+        //asses inputs and determine target - move to seperate function
+
+
+        state.directionX = state.rightStickX / std::abs(state.rightStickX);
+
+        state.straightTarget = -state.leftStickY;
+        state.turnTarget = std::pow(state.rightStickX, 2) * state.directionX * DriveConstants::kArcTurnMultipler;
+
+        if (std::abs(state.leftStickY) < DriveConstants::kDeadbandY) {
+            state.straightTarget = 0;
+        }
+
+        if (std::abs(state.rightStickX) < DriveConstants::kDeadbandX) {
+            state.turnTarget = 0;
+        }
+
+        state.currentLeftTarget = state.straightTarget + state.turnTarget;
+        state.currentRightTarget = state.straightTarget - state.turnTarget;
+    }
+    // rocket league
+    else {
+        state.directionX = (state.leftStickX >= 0) ? -1 : 1;
+        state.directionY = (state.leftTrigger - state.rightTrigger >= 0) ? 1 : -1;
+        state.boostMultiplier = (state.yButton) ? DriveConstants::kBoost : DriveConstants::kNoBoost;
+
+        state.straightTarget = -std::pow((state.leftTrigger - state.rightTrigger), 2) * state.directionY * state.boostMultiplier * DriveConstants::kDriveMultiplierY;
+        state.turnTarget = -std::pow((state.leftStickX * DriveConstants::kDriveMultiplierX), 2) * state.directionX;
+
+        if (std::abs(state.leftStickX) < DriveConstants::kDeadbandX) {
+            state.turnTarget = 0;
+        }
+
+        if (std::abs(state.leftTrigger - state.rightTrigger) < DriveConstants::kDeadbandY) {
+            state.straightTarget = 0;
+        }
+
+        // if (!(state.straightTarget == 0) && state.turnTarget == 0) {
+        //     state.turnTarget = (leftCANEncoder.GetVelocity() - rightCANEncoder.GetVelocity()) * DriveConstants::kDriveOffset;
+        // }
+
+        state.currentLeftTarget = state.straightTarget + state.turnTarget;
+        state.currentRightTarget = state.straightTarget - state.turnTarget;
+    }
+
+    if (state.drivetrainState == DrivetrainState::MANUAL) {
+        leftDriveLead.Set(state.currentLeftTarget);
+        leftDriveFollow.Set(state.currentLeftTarget);
+        rightDriveLead.Set(state.currentRightTarget);
+    }
+    else if (state.drivetrainState == DrivetrainState::DISABLED) {
+        setPower(0);
+        state.currentLeftTarget = 0;
+        state.currentRightTarget = 0;
+    }
+}
+
+
+
+void Drivetrain::resetState() {
+
+}
+
+void Drivetrain::setPower(double power) {
+    leftDriveLead.Set(power);
+    rightDriveLead.Set(power);
+}
---
+++ Competition-2021/src/main/include/Constants.h
@@ -0,0 +1,47 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+/**
+ * The Constants header provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants.  This should not be used for any other
+ * purpose.
+ *
+ * It is generally a good idea to place constants into subsystem- or
+ * command-specific namespaces within this header, which can then be used where
+ * they are needed.
+ */
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace OIConstants {
+    constexpr static int GAMEPAD_BASE_LOCATION = 1;
+    constexpr static int GAMEPAD_OPERATOR_LOCATION = 0;
+}
+
+namespace DriveConstants {
+    constexpr static int CAN_ID_LEFT_A = 1;
+    constexpr static int CAN_ID_LEFT_B = 3;
+    constexpr static int CAN_ID_RIGHT_A = 2;
+    constexpr static int CAN_ID_RIGHT_B = 4;
+
+    constexpr static double kDeadbandX = 0.05;
+    constexpr static double kDeadbandY = 0.1;
+    constexpr static double kArcTurnMultipler = 0.5;
+    constexpr static double kNoBoost = 0.5;
+    constexpr static double kBoost = 1;
+    constexpr static double kDriveMultiplierX = 0.75;
+    constexpr static double kDriveMultiplierY = 1;
+}
+
+namespace SpindexerConstants {
+    constexpr static int CAN_ID = 7;
+}
+
+#endif
