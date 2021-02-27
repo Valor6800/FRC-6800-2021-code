d1c613e1d6357f845e5e99c3aa0af1a6b2a28188
Michael Ray <mray190@gmail.com>
2021-02-27 14:43:09 -0600

    Finalized CAN IDs - optimized for fuse locations

--- Competition/src/main/include/Constants.h
+++ Competition/src/main/include/Constants.h
@@ -91,9 +91,9 @@ namespace DriveConstants {
 }
 
 namespace ShooterConstants {
-    constexpr static int CAN_ID_FLYWHEEL_A = 11; //changed to ID=1 for testing
-    constexpr static int CAN_ID_FLYWHEEL_B = 12;
-    constexpr static int CAN_ID_TURRET = 6;
+    constexpr static int CAN_ID_FLYWHEEL_A = 8;
+    constexpr static int CAN_ID_FLYWHEEL_B = 10;
+    constexpr static int CAN_ID_TURRET = 7;
     constexpr static int SOLENOID_ID_SHOOTER = 1;
 
     // Deadband for the left joystick x axis
@@ -138,9 +138,9 @@ namespace ShooterConstants {
 }
 
 namespace SpindexerConstants {
-    constexpr static int CAN_ID = 10;
-    constexpr static int CAN_ID_THROAT = 5;
-    constexpr static int CAN_ID_THROAT_FOLLOW = 7;
+    constexpr static int CAN_ID = 13;
+    constexpr static int CAN_ID_THROAT = 11;
+    constexpr static int CAN_ID_THROAT_FOLLOW = 12;
 
     constexpr static double left_trigger_deadband = 0.05;
     constexpr static double default_throat_spd = 0.6;
@@ -149,8 +149,8 @@ namespace SpindexerConstants {
 }
 
 namespace LiftConstants {
-    constexpr static int MOTOR_CAN_ID = 9;
-    constexpr static int MOTOR_FOLLOW_CAN_ID = 13;
+    constexpr static int MOTOR_CAN_ID = 5;
+    constexpr static int MOTOR_FOLLOW_CAN_ID = 6;
 
     constexpr static int LIMIT_DIO = 0;
     constexpr static int POT_ANOLOG_PORT = 0;
@@ -162,7 +162,7 @@ namespace LiftConstants {
 namespace IntakeConstants {
     constexpr static int DEFAULT_ROLLER_SPD = 0.6;
 
-    constexpr static int MOTOR_CAN_ID = 12; //change to correct value
+    constexpr static int MOTOR_CAN_ID = 14;
 
     constexpr static int SOLENOID_FORWARD_PCM_CAN_ID = 0;
 }
