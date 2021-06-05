b2e7d2ee1e0b5eec15a7226cdc6e72218a86bb20
j a <bj.texasaldens@gmail.com>
2021-06-05 15:04:35 -0500

    working gyro changes and drive pathing

--- Competition/src/main/cpp/ValorAuto.cpp
+++ Competition/src/main/cpp/ValorAuto.cpp
@@ -7,8 +7,8 @@ ValorAuto::ValorAuto(Drivetrain* _drivetrain, Intake* _intake, Shooter* _shooter
         spindexer(_spindexer),
         kDriveKinematics{DriveConstants::kTrackwidth},
         kSimpleMotorFeedforward{RamseteConstants::kS, RamseteConstants::kV, RamseteConstants::kA},
-        kTrajectoryConfigForward{RamseteConstants::kMaxSpeed, RamseteConstants::kMaxAcceleration},
-        kTrajectoryConfigReverse{RamseteConstants::kMaxSpeed, RamseteConstants::kMaxAcceleration},
+        kTrajectoryConfigForward{RamseteConstants::kMaxForwardSpeed, RamseteConstants::kMaxAcceleration},
+        kTrajectoryConfigReverse{RamseteConstants::kMaxReverseSpeed, RamseteConstants::kMaxAcceleration},
         kDifferentialDriveVoltageConstraint{kSimpleMotorFeedforward, kDriveKinematics, 10_V} {
 
     kTrajectoryConfigForward.SetKinematics(kDriveKinematics);
@@ -28,17 +28,36 @@ ValorAuto::ValorAuto(Drivetrain* _drivetrain, Intake* _intake, Shooter* _shooter
     frc2::InstantCommand cmd_shoot = frc2::InstantCommand( [&] {
         spindexer->state.drumState = Spindexer::DrumState::HIGH;
     });
+    frc2::InstantCommand cmd_track = frc2::InstantCommand( [&] {
+        shooter->state.turretState = Shooter::TurretState::TRACK;
+    });
     frc2::InstantCommand cmd_stopShoot = frc2::InstantCommand( [&] {
         spindexer->state.drumState = Spindexer::DrumState::LOW;
     });
 
 
     auto traj_move5 = frc::TrajectoryGenerator::GenerateTrajectory(frc::Pose2d(0_m, 0_m,frc::Rotation2d(0_deg)),
-                                                                    { frc::Translation2d(1_m, 0.3_m)},
-                                                                    frc::Pose2d(2_m, 0.3_m, frc::Rotation2d(0_deg)),
+                                                                    { frc::Translation2d(2_m, 1.6_m)},
+                                                                    frc::Pose2d(5.6_m, 2_m, frc::Rotation2d(0_deg)),
                                                                     kTrajectoryConfigForward);
 
+    auto traj_reverse_move5 = frc::TrajectoryGenerator::GenerateTrajectory(frc::Pose2d(5.6_m, 2_m, frc::Rotation2d(0_deg)),
+                                                                    { },
+                                                                    frc::Pose2d(0_m, 0_m, frc::Rotation2d(0_deg)),
+                                                                    kTrajectoryConfigReverse);
+
     frc2::RamseteCommand cmd_move5(traj_move5,
+                                    [&] () { return drivetrain->GetPose(); },
+                                    frc::RamseteController(RamseteConstants::kRamseteB, RamseteConstants::kRamseteZeta),
+                                    kSimpleMotorFeedforward,
+                                    kDriveKinematics,
+                                    [&] { return drivetrain->GetWheelSpeeds(); },
+                                    frc2::PIDController(RamseteConstants::kPDriveVel, 0, 0),
+                                    frc2::PIDController(RamseteConstants::kPDriveVel, 0, 0),
+                                    [&] (auto left, auto right) { drivetrain->TankDriveVolts(left, right); },
+                                    {drivetrain});
+
+    frc2::RamseteCommand cmd_reverse_move5(traj_reverse_move5,
                                     [&] () { return drivetrain->GetPose(); },
                                     frc::RamseteController(RamseteConstants::kRamseteB, RamseteConstants::kRamseteZeta),
                                     kSimpleMotorFeedforward,
@@ -55,9 +74,15 @@ ValorAuto::ValorAuto(Drivetrain* _drivetrain, Intake* _intake, Shooter* _shooter
                              cmd_intake,
                              cmd_shoot,
                              frc2::WaitCommand((units::second_t)2),
-                             cmd_spoolDown,
                              cmd_stopShoot,
-                             std::move(cmd_move5));
+                             std::move(cmd_move5),
+                             std::move(cmd_reverse_move5),
+                             cmd_track,
+                             frc2::WaitCommand((units::second_t)1),
+                             cmd_shoot,
+                             frc2::WaitCommand((units::second_t)2),
+                             cmd_stopShoot,
+                             cmd_spoolDown);
     autos["Shoot3Move5"] = shoot3move5;
 
 }
