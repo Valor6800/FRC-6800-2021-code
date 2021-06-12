e36a5fa31a2e1fe220f180be4053f0aca335c964
j a <bj.texasaldens@gmail.com>
2021-06-12 18:07:44 -0500

    shooter rpm tuning

--- Competition/src/main/include/Constants.h
+++ Competition/src/main/include/Constants.h
@@ -120,8 +120,8 @@ namespace ShooterConstants {
     // initiation: shooter 65%
     // trench: 70%
     constexpr static int fenderPower = 4000; // 3200-3300
-    constexpr static int initiationPower = 4200; // 2800-2900
-    constexpr static int trenchPower = 4000; // 3200- 3300
+    constexpr static int initiationPower = 3900; // 2800-2900
+    constexpr static int trenchPower = 3900; // 3200- 3300
     constexpr static int defaultManualPower = 3500; // 3200
 
     constexpr static double shooterKP = 0.001;
